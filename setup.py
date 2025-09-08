0ea9738ba9481feedc298422b5629430daa3b81a
Valmir França <vfranca3@gmail.com>
2025-09-08 03:23:33 -0300

    Alterando configuracoes para o RTD

--- setup.py
+++ setup.py
@@ -1,19 +1,4 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
-setup(
-    name="mtcli-volume",
-    version="1.0.2",
-    description="Plugin mtcli para exibir o volume profile",
-    author="Valmir França da Silva",
-    author_email="vfranca3@gmail.com",
-    packages=find_packages(),
-    install_requires=[
-        "mtcli",
-    ],
-    entry_points={
-        "mtcli.plugins": [
-            "mtcli-volume = mtcli_volume.plugin:volume",
-        ],
-    },
-    python_requires='>=3.10,<3.14',
-)
+
+setup()
