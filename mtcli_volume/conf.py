97132421f3ac9c839ad9924d19cf6ce7d5341fb6
Valmir França <vfranca3@gmail.com>
2025-09-09 08:55:13 -0300

    Corrigido o tratamento da configuracao digitos

--- mtcli_volume/conf.py
+++ mtcli_volume/conf.py
@@ -3,6 +3,6 @@ from mtcli.conf import config
 
 
 SYMBOL = os.getenv("SYMBOL", config["DEFAULT"].get("symbol", fallback="WIN$N"))
-DIGITOS = os.getenv("DIGITOS", config["DEFAULT"].getint("digitos", fallback=0))
-PERIODS = os.getenv("PERIODS", config["DEFAULT"].getint("periods", fallback=566))
-STEP = os.getenv("STEP", config["DEFAULT"].getfloat("step", fallback=100))
+DIGITOS = int(os.getenv("DIGITOS", config["DEFAULT"].getint("digitos", fallback=0)))
+PERIODS = int(os.getenv("PERIODS", config["DEFAULT"].getint("periods", fallback=566)))
+STEP = float(os.getenv("STEP", config["DEFAULT"].getfloat("step", fallback=100)))
